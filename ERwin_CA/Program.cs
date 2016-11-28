7445ef886ff42af2a328b422e0644bb3c9a5c663
Antonino Amato <antonino.amato@we-plus.eu>
2016-11-28 09:10:50 +0100

    Add project files.

---
+++ ERwin_CA/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERwin_CA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
